ab6539a994622be41c3db1c353016ccf31273261
ⵟⴰⵕⵉⴽ ⴱⴰⴽⵉⵕ <41262131+TRKBKR@users.noreply.github.com>
2019-10-13 20:56:59 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+
+
+clear
+echo "					Installing ... "
+sudo cp symbols/* /usr/share/X11/xkb/symbols/
+sudo cp rules/* /usr/share/X11/xkb/rules/
+echo "\033[31m=====> : Installed Sucessfully \033[39m"
+echo "\033[32mSetting Up Keyboard ...\033[39m"
+sleep 1
+default="$(setxkbmap -query |grep layout |awk '{print$2}')"
+setxkbmap -option grp:alt_shift_toggle $default,mz
+
+echo "Done"
+
+echo "       To change ALT+SHIFT"
